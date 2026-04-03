a1fe712d5a4ec6b5aebcb21a70e8c05e2aa0ede8
Nevio Pongiluppi <nevio-src@clavitus.ch>
2026-04-03 17:42:21 +0200

    made ouput better looking

--- get_buddah/Program.cs
+++ get_buddah/Program.cs
@@ -39,10 +39,31 @@ if (ping() == "down")
 
 
 var json = await http.GetStringAsync("https://buddha-api.com/api/random");
-
 using var doc = JsonDocument.Parse(json);
 
+
 string text = doc.RootElement.GetProperty("text").GetString();
+int string_length = text.Length;
+
+
+
+string buddah = """
+                   _
+                  (")
+                 (-"-)
+                (_`/._)
+                
+                """;
+
+string border = new string('-', string_length);
+
+Console.WriteLine(border);
+
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine(text);
+Console.ResetColor();
 
+Console.WriteLine(border);
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(buddah);
